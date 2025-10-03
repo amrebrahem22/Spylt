7573968fba012f0f27101e74474899e80b80be3a
unknown <amrebrahem226@gmail.com>
2025-10-03 16:48:30 +0300

    feat: Add Hero Section

---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section className="bg-main-bg">
+      <div className="hero-container">
+        <img
+          src="/images/static-img.png"
+          alt="static-hero"
+          className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
+        />
+
+        <div className="hero-content">
+          <div className="overflow-hidden">
+            <h1 className="hero-title">Freaking Delicious</h1>
+          </div>
+          <div
+            style={{
+              clipPath: "polygon(50% 0, 50% 0, 50% 100%, 50% 100%)",
+            }}
+            className="hero-text-scroll"
+          >
+            <div className="hero-subtitle">
+              <h1>Protein + Caffine</h1>
+            </div>
+          </div>
+
+          <h2>
+            Live life to the fullest with Spylt: Shatter boredom and embrace
+            your inner kid with with every deliciously smooth chug.
+          </h2>
+
+          <div className="hero-button">
+            <p>Chug a SPYLT</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HeroSection;
