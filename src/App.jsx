1949bd6cd375552e31eb13c5965e075ed68ff902
unknown <amrebrahem226@gmail.com>
2025-10-03 20:14:38 +0300

    style: add gsap animations to hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
       <HeroSection />
+      <div className="min-h-screen"></div>
     </main>
   );
 };
--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,6 +1,66 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 import React from "react";
 
 const HeroSection = () => {
+  useGSAP(() => {
+    const titleSplit = SplitText.create(".hero-title", {
+      type: "chars",
+    });
+
+    const tl = gsap.timeline({
+      delay: 1,
+    });
+
+    tl.to(".hero-content", {
+      opacity: 1,
+      y: 0,
+      ease: "power1.inOut",
+    })
+      .to(
+        ".hero-text-scroll",
+        {
+          duration: 1,
+          clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+          ease: "circ.out",
+        },
+        "-=0.5"
+      )
+      .from(
+        titleSplit.chars,
+        {
+          yPercent: 200,
+          stagger: 0.02,
+          ease: "power2.out",
+        },
+        "-=0.5"
+      );
+
+    const heroTL = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-container",
+        start: "1% top",
+        end: "bottom top",
+        scrub: true,
+      },
+    });
+
+    heroTL.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+
+    heroTL.to(".hero-container", {
+      rotate: 7,
+      scale: 0.9,
+      yPercent: 30,
+      ease: "power1.inOut",
+    });
+  });
+
   return (
     <section className="bg-main-bg">
       <div className="hero-container">
@@ -10,7 +70,7 @@ const HeroSection = () => {
           className="absolute bottom-0 left-1/2 -translate-x-1/2 object-auto scale-100 md:scale-150"
         />
 
-        <div className="hero-content">
+        <div className="hero-content opacity-0">
           <div className="overflow-hidden">
             <h1 className="hero-title">Freaking Delicious</h1>
           </div>
@@ -27,7 +87,7 @@ const HeroSection = () => {
 
           <h2>
             Live life to the fullest with Spylt: Shatter boredom and embrace
-            your inner kid with with every deliciously smooth chug.
+            your inner kid with every deliciously smooth chug.
           </h2>
 
           <div className="hero-button">
