1f4452fa7005ec6ac93f894030a0da540af382ec
unknown <amrebrahem226@gmail.com>
2025-10-03 15:42:30 +0300

    feat: Add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+
+const Navbar = () => {
+  return (
+    <nav className="fixed top-0 left-0 z-50 md:p-9 p3">
+      <img src="/images/nav-logo.svg" alt="nav-logo" className="w-20 md:w-24"/>
+    </nav>
+  );
+};
+
+export default Navbar;
